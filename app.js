e5fbb15b123a5fc6a3f7d1b801fbdc6bfd7978f8
Marc Lafita Forcada <lafitadev@gmail.com>
2013-12-28 21:50:51 +0100

    Afegida la pagina del vademecum Afegit el formulari de login

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 
 var express = require('express');
 var routes = require('./routes');
+var vademecum = require('./routes/vademecum');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
@@ -31,6 +32,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/vademecum', vademecum.vademecum);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ routes/vademecum.js
@@ -0,0 +1,6 @@
+/**
+ * New node file
+ */
+exports.vademecum = function(req, res){
+  res.render('vademecum', { title: 'Express' });
+};
